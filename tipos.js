69d776f4398de4c8aa429a25bf72bd73bce6a452
RodolfoReis <75094857+RodolfoGDS@users.noreply.github.com>
2022-08-02 11:28:42 -0300

    Add files via upload

---
+++ tipos.js
@@ -0,0 +1,56 @@
+// Tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica) 
+
+// ponto flutuante
+
+const numeroFlutuante = 3.3;
+const numeroFlutuantesemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroFlutuante;
+
+console.log(novaOperacao)
+
+
+// NaN --> Not a Number (não é um número)
+
+const nome = "Rodolfo";
+
+console.log(nome * primeiroNumero)
+
+
+// tipo string - texto
+
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const texto3 = "SenhaSuperSegura123";
+const stringNumeros = "1234";
+const citacao = "Eu vou fazer uma citação 'sou feliz' eu disse!";
+const meuNome = "Rodolfo";
+const apresentacao = "Olá meu nome é: ";
+console.log(texto1,texto2,texto3,stringNumeros,citacao);
+
+// concatenação (+)
+
+console.log(apresentacao + meuNome);
+
+// tipo booleano
+
+const numero1 = 5;
+const numero2 = 10;
+let cadastroAtivado = true;
+
+console.log(numero1 === numero2);
+
+const textoA = "Rodolfo"
+const textoB = "Reis"
+const textoC ="rodolfo"
+console.log(textoA === textoB)
